d622eea39fd1b503a8a964ea1be9b24768358c37
SrushtiPatil1 <83879917+SrushtiPatil1@users.noreply.github.com>
2022-04-04 15:18:11 +0530

    Update history.py

--- history.py
+++ history.py
@@ -7,34 +7,7 @@ cur = con.cursor()
 cur.execute("select url, title, visit_count, last_visit_time from urls")
 results = cur.fetchall()
 rows = []
-
-def parse(url):
-	try:
-		parsed_url_components = url.split('//')
-		sublevel_split = parsed_url_components[1].split('/', 1)
-		domain = sublevel_split[0].replace("www.", "")
-		return domain
-	except IndexError:
-		print ("URL format error!")
-
-
 for result in results:
-     def parse(url):
-          parsed_url_components = url.split('//')
-          sublevel_split = parsed_url_components[1].split('/', 1)
-          domain = sublevel_split[0].replace("www.", "")
-          return domain
-
-     sites_count = {} #dict makes iterations easier :D
-
-     for url, count in results:
-          url = parse(url)
-          if url in sites_count:
-               sites_count[url] += 1
-          else:
-               sites_count[url] = 1
-	     
-
      print(result)
      rows.append(result) 
      fields = ['url', 'title', 'visit_count', 'last_visit_time']  
